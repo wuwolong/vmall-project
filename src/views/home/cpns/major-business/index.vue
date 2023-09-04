e2352ead3e96b3676da504f34ed86efefa4a65e3
wuwolong <673864464@qq.com>
2023-09-04 15:43:05 +0800

    feat: finished major business

---
+++ src/views/home/cpns/major-business/index.vue
@@ -0,0 +1,73 @@
+<script lang="ts" setup>
+import searchIcon from '@/assets/imgs/search-icon.vue'
+</script>
+
+<template>
+  <div class="major-business">
+    <div class="wrapper">
+      <div class="logo">
+        <a href="#">
+          <img
+            src="https://res.vmallres.com/portal/1.23.8.300/h5/images/logo_app.png"
+            alt=""
+          />
+        </a>
+      </div>
+      <ul class="category">
+        <li>
+          <a href="#">华为</a>
+        </li>
+        <li>
+          <a href="#">AITO汽车</a>
+        </li>
+        <li>
+          <a href="#">华为智选</a>
+        </li>
+        <li>
+          <a href="#">生态周边</a>
+        </li>
+      </ul>
+      <div class="search">
+        <searchIcon />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.major-business {
+  .wrapper {
+    height: 74px;
+    /* background-color: red; */
+    display: flex;
+    align-items: center;
+    .logo {
+      a {
+        img {
+          height: 74px;
+        }
+      }
+    }
+    .category {
+      flex: 1;
+      padding: 0 30px;
+      display: flex;
+      li {
+        margin-left: 30px;
+        a {
+          color: #000;
+          font-size: 16px;
+          &:hover {
+            color: $hoverColor;
+          }
+        }
+      }
+    }
+    .search {
+      flex: 0 0 40px;
+      align-self: center;
+      text-align: right;
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,11 @@
 <template>
   <nav-bar></nav-bar>
+  <major-business></major-business>
 </template>
 
 <script lang="ts" setup>
 import navBar from './cpns/nav-bar/index.vue'
+import majorBusiness from './cpns/major-business/index.vue'
 </script>
 
 <style lang="scss" scoped></style>
